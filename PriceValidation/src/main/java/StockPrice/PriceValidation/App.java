3f76bae2587149118afe1470c9f4ad75e7630a78
Pushpendra-01 <128477555+Pushpendra-01@users.noreply.github.com>
2024-04-15 18:20:02 +0530

    Add files via upload

---
+++ PriceValidation/src/main/java/StockPrice/PriceValidation/App.java
@@ -0,0 +1,13 @@
+package StockPrice.PriceValidation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
